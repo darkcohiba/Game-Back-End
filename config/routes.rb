ad88f6fac7e4bdcab0760bec7473ba2e55468547
Sam <samuel.waters@gmail.com>
2022-05-05 12:50:16 -0600

    base

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # Login / Logout Routes
+  post '/login', to: 'sessions#login'
+  delete '/logout', to: 'sessions#logout'
 end
---
+++ db/migrate/20220505170354_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.boolean :admin, default: false
+      t.string :email
+      t.integer :flappy_high_score, default: 0
+      t.integer :dodo_high_score, default: 0
+      t.datetime "created_at", precision: 6, null: false
+      t.datetime "updated_at", precision: 6, null: false
+      t.string "password_digest"
+    end
+  end
+end
